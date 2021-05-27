aed4fcf3d64b3295a23dc29e678a8c1f5d71bdfc
Nofarius1992 <tolya.prozhoga@gmail.com>
2021-05-27 07:01:34 +0300

    Completed 3rd lesson

---
+++ install.sh
@@ -0,0 +1,15 @@
+bin/magento setup:install \
+--base-url=http://m242os.mg.loc \
+--db-host=localhost \
+--db-name=m242os \
+--db-user=nofarius \
+--db-password=nofariusamdm \
+--admin-firstname=admin \
+--admin-lastname=admin \
+--admin-email=admin@admin.com \
+--admin-user=admin \
+--admin-password=admin123 \
+--language=en_US \
+--currency=USD \
+--timezone=America/Chicago \
+--use-rewrites=1
