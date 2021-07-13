e9c4609b6cfcd1c0a30985d5c0d55ed7ee054b39
Nofarius1992 <tolya.prozhoga@gmail.com>
2021-07-13 06:27:12 +0300

    Completed the 32nd lesson. Created the first task

--- app/code/MageMastery/Todo/Controller/Index/Index.php
+++ app/code/MageMastery/Todo/Controller/Index/Index.php
@@ -2,13 +2,41 @@
 
 namespace MageMastery\Todo\Controller\Index;
 
-use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\ActionInterface;
 use Magento\Framework\Controller\ResultFactory;
+use MageMastery\Todo\Model\Task;
+use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
+use MageMastery\Todo\Model\TaskFactory;
 
-class Index extends Action
+class Index implements ActionInterface, HttpGetActionInterface
 {
+    private ResultFactory $resultFactory;
+    private $taskResource;
+    private $taskFactory;
+
+    public function __construct(
+        ResultFactory $resultFactory,
+        TaskFactory $taskFactory,
+        TaskResource $taskResource
+    ) {
+        $this->resultFactory = $resultFactory;
+        $this->taskFactory = $taskFactory;
+        $this->taskResource = $taskResource;
+    }
+
     public function execute()
     {
+        $task = $this->taskFactory->create();
+        $task->setData([
+            'label' => 'New Task 22',
+            'status' => 'open',
+            'customer_id' => 1
+        ]);
+
+        $this->taskResource->save($task);
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
+
+
