c77c03682b493a99158d3b2cfc60c808715254d7
Nofarius1992 <tolya.prozhoga@gmail.com>
2021-07-15 06:36:21 +0300

    Completed the 34th lesson. Created an implementation of the task return method

--- app/code/MageMastery/Todo/Controller/Index/Index.php
+++ app/code/MageMastery/Todo/Controller/Index/Index.php
@@ -2,6 +2,7 @@
 
 namespace MageMastery\Todo\Controller\Index;
 
+use MageMastery\Todo\Service\TaskRepository;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\App\ActionInterface;
 use Magento\Framework\Controller\ResultFactory;
@@ -12,29 +13,41 @@ use MageMastery\Todo\Model\TaskFactory;
 class Index implements ActionInterface, HttpGetActionInterface
 {
     private ResultFactory $resultFactory;
+
     private $taskResource;
+
     private $taskFactory;
+
+    /**
+     * @var TaskRepository
+     */
+    private $taskRepository;
 
     public function __construct(
         ResultFactory $resultFactory,
         TaskFactory $taskFactory,
-        TaskResource $taskResource
+        TaskResource $taskResource,
+        TaskRepository $taskRepository
     ) {
         $this->resultFactory = $resultFactory;
         $this->taskFactory = $taskFactory;
         $this->taskResource = $taskResource;
+        $this->taskRepository = $taskRepository;
     }
 
     public function execute()
     {
-        $task = $this->taskFactory->create();
-        $task->setData([
-            'label' => 'New Task 22',
-            'status' => 'open',
-            'customer_id' => 1
-        ]);
+        $task = $this->taskRepository->get(1);
+        var_dump($task->getData());
 
-        $this->taskResource->save($task);
+//        $task = $this->taskFactory->create();
+//        $task->setData([
+//            'label' => 'New Task 22',
+//            'status' => 'open',
+//            'customer_id' => 1
+//        ]);
+//
+//        $this->taskResource->save($task);
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
